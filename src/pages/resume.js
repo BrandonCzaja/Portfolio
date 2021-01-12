7491d69fe0202e18753c70d155797f292b43988b
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-12 13:16:46 -0600

    Added Resume Link

---
+++ src/pages/resume.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import Layout from "../layout/Layout"
+import Projects_Images from "../layout/projects"
+
+const Resume = () => {
+  return (
+    <>
+      <Layout>
+        <div id="resume" name="resume">
+          <iframe src="https://docs.google.com/document/d/e/2PACX-1vQpCC2u5cuHynbBBMXQWS33f5xg1S6dhZJ0yv0NMYVF8jGjafKbdG2paMQFJ3sLQuo5jg4BFCUuNcXk/pub?embedded=true">
+            Resume
+          </iframe>
+        </div>
+      </Layout>
+    </>
+  )
+}
+
+export default Resume
